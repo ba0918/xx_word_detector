872551417687655f0d26299fd03bd9126376c653
ba0918 <pub.dev.delusions@gmail.com>
2017-05-25 01:04:55 +0900

    Update filters

---
+++ src/filters/Filterable.js
@@ -0,0 +1,8 @@
+/* @flow */
+
+export interface Filterable<T> {
+  execute(input: T): T
+}
+
+export interface NumericFilterable extends Filterable<number> {}
+export interface StringFilterable extends Filterable<string> {}
